9ac6d3905c83183addc384f35d1a2ae1f1bd714c
Max Kraanen <max@maxkraanen.com>
2020-04-10 11:27:03 +1200

    added gitignore

---
+++ source/javascripts/application.js
@@ -0,0 +1,4 @@
+//= require_tree .
+
+//= require navbar
+//= require carous
---
+++ source/javascripts/navbar.js
@@ -0,0 +1,14 @@
+
+  $(document).ready(function() {
+    // Transition effect for navbar
+    $(window).scroll(function() {
+    if($(this).scrollTop() > 71) {
+      $('.navbar').addClass('solid');
+        } else {
+      $('.navbar').removeClass('solid');
+        }
+      });
+   });
+
+
+
